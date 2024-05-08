35a2b61e756beb62b21ba6e467b84bebe068be5e
Thiman Krishly <thiman26rbn@gmail.com>
2024-05-08 17:37:20 +0100

    Challenge maze solution

---
+++ Day-6-Functions/hurdle_3.py
@@ -0,0 +1,23 @@
+# Solution for Reeborg's World hurdle 3. https://reeborg.ca/index_en.html
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+            
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
